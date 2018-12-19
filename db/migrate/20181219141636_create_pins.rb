43ff8381b96f83ecd526cc03c754f3a2f867ceae
cristianahue666 <cristian.ahumadae9@gmail.com>
2018-12-19 12:11:01 -0300

    pins

---
+++ db/migrate/20181219141636_create_pins.rb
@@ -0,0 +1,11 @@
+class CreatePins < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pins do |t|
+      t.string :photo
+      t.string :titulo
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
